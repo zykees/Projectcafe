2a91fd863c4921d2d80e3c3b9aff358796fba0bb
SeekkyZ <phongsakorn547@gmail.com>
2025-05-19 23:23:58 +0700

    Revert "Merge branch 'API'"
    This reverts commit f859030ce320130c94c9be6b7f22117616523adb, reversing
    changes made to 85a468f17815838ecdf48fca0af70d5c40266148.

--- LineLogin/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('LINE_CHANNEL_ID', '2007056322');
-define('LINE_CHANNEL_SECRET', '7067df184a82334cb7b0c7753124c83d');
-define('LINE_CALLBACK_URL', 'http://localhost/Projectcafe/LineLogin/callback.php');
